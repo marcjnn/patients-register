a1c1e063d1e0cf571c09b6d2b0b28946fb88b933
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-05-01 11:16:17 +0200

    create basic html structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,51 @@
 <template>
   <div id="app">
-    <div id="nav"><router-link to="/">Home</router-link> |</div>
-    <router-view />
+    <header>Dr. Pablo Kehyaian</header>
+    <main>
+      <aside></aside>
+      <section>
+        <!-- Listado de pacientes as variable? -->
+        <h2>Listado de pacientes</h2>
+        <article>
+          icon
+          <p>Listado de pacientes</p>
+          <small>visualizacion de pacientes</small>
+        </article>
+        <input type="text" />
+        <button>icon Nuevo paciente</button>
+        <button>Descargar CSV</button>
+        <ul>
+          <li>icon</li>
+          <li>icon</li>
+          <li>5</li>
+          <li>10</li>
+          <li>15</li>
+        </ul>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Nombre y apellidos icon</th>
+              <th>Clinica icon</th>
+              <th>Objetivo tratamiento icon</th>
+              <th>Estado icon</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- v-for with patients -->
+            <tr>
+              <td>img + name + dob</td>
+              <td>Clinica dental</td>
+              <td>Estética y Oclusión</td>
+              <td>Facturado</td>
+              <td>Acciones drop-down</td>
+            </tr>
+          </tbody>
+        </table>
+        <nav>pagination</nav>
+      </section>
+    </main>
   </div>
 </template>
 
